b363149d093d127e867341cad9f9acd3c28551c9
antickbhattacharjee <antickbh15122001@gmail.com>
2026-03-16 14:50:29 +0530

    Updater settings.py

--- config/settings.py
+++ config/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-om+u98=)i6pkuw547q!0@#$*v$24fnu@xor#1q8=o$@%-y$@=t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["geo-attendance-sy7e.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
